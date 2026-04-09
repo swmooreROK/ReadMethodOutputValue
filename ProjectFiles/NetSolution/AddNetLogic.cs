db79a042b6fa2f82d17427d0e9522e865cfdde90
swmoore <swmoore@rockwellautomation.com>
2026-04-09 14:49:17 -0500

    initial commit

---
+++ ProjectFiles/NetSolution/AddNetLogic.cs
@@ -0,0 +1,29 @@
+#region Using directives
+using System;
+using UAManagedCore;
+using OpcUa = UAManagedCore.OpcUa;
+using FTOptix.HMIProject;
+using FTOptix.UI;
+using FTOptix.Retentivity;
+using FTOptix.NativeUI;
+using FTOptix.Core;
+using FTOptix.CoreBase;
+using FTOptix.NetLogic;
+#endregion
+
+public class AddNetLogic : BaseNetLogic
+{
+    public override void Start()
+    {
+        // Insert code to be executed when the user-defined logic is started
+    }
+
+    public override void Stop()
+    {
+        // Insert code to be executed when the user-defined logic is stopped
+    }
+    [ExportMethod]
+    public void AddNumbers(int number1, int number2, out int result) {
+        result = number1 + number2;
+    }
+}
